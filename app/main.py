727d423ba420300f82f7430535659fd87700cca7
mdeclerk <matthias.de.clerk@gmx.de>
2026-01-03 07:27:45 +0100

    Update application title to "WebChess MVP"

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 
 from app.api.routes import router as api_router
 
-app = FastAPI(title="OpenAI Chess MVP")
+app = FastAPI(title="WebChess MVP")
 
 app.include_router(api_router, prefix="/api")
 
